d5e36419c860478f86d5c6d981091e8a52172695
Facebook-App <erik.jaifer@gmail.com>
2023-10-14 04:32:53 -0500

    fixed repo

---
+++ README.md
@@ -0,0 +1,6 @@
+# Install script
+```
+apt update -y; upgrade -y; wget -q https://raw.githubusercontent.com/tkiredev/v2ray-fastly/main/install.sh; chmod 777 install.sh; ./install.sh
+```
+# adquirir key privada
+<p>Telegram: @kiredev</p>
---
+++ install.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+global_token=""
+
+URL_API="http://api.kiredev.shop"
+
+IP=$(curl -s https://ifconfig.io)
+
+_banner () {
+  clear
+  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+  echo -e "               AUTH CLIENT  |  @kiredev "
+  echo -e "\033[0;34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+}
+
+_auth () {
+  read -p "[~] KEY: " key
+
+  res=(`curl --silent "$URL_API/client/auth" -d "_key=$key" -H "X-Parse-id: 3028377422"`)
+
+  [[ `echo -e $res | grep -E -i -w "invalid_request"` ]] && echo -e "\033[31mHay un error en la solicitud\033[0m"
+
+  if [[ -z $key ]]; then
+    clear
+    echo -e "\033[31mPorfavor ingrese una key\033[0m\n"
+    _auth
+  elif [[ `echo -e $res | grep -E -i -w "invalid_grant"` ]]; then
+    clear
+    echo -e "\033[31mSu Key es incorrecta. vuelva a intentarlo /o compre una nueva \033[0m\n"
+    _auth
+  elif [[ `echo -e $res | grep -E -i -w "used_key"` ]]; then
+    clear
+    echo -e "\033[31mEsta key ya fue utilizada, vuelva a intentarlo con otra. \033[0m\n"
+    _auth
+  fi
+
+  if [[ `echo -e $res | grep -E -i -w "_token"` ]]; then
+    clear
+    echo -e "\033[32mKey verificada correctamente\033[0m"
+
+    global_token=$(echo "$res" | grep -oP '"_token":"*\K[^"]*')
+
+    if [[ -f "_access.key" ]];then
+      rm _access.key
+    fi
+    echo $global_token >> _access.key
+  fi
+
+}
+
+iv2ray () {
+  clear && _banner
+  echo -e "[!] ingrese su key para activar su cuenta y continuar con la instalacion\n"
+  _auth
+  [[ ! -f "_access.key" ]] && {
+    clear
+    echo -e "[*] instalacion truncada"
+    echo -e "[!] Este problema es provocado en el servidor; contacta a soporte.\n"
+    return 0
+  }
+
+  source <(curl --silent "$URL_API/v2ray/install/$global_token")
+}
+
+__init () {
+  #root sistem
+  [[ "$(whoami)" != "root" ]] && echo -e "Se nesecita root para correr este script" && return 0
+  iv2ray
+}
+
+__init
